e6b7bb636d40276bfc1b0b5cb8570e223a34fd25
To grivel <grivel.theo@protonmail.com>
2023-01-05 15:09:08 +0000

    map: defautl constructor

--- headers/Map.hpp
+++ headers/Map.hpp
@@ -8,7 +8,12 @@ template <typename T1, typename T2> class map
 	typedef ft::pair<T1, T2> pair;
 
   public:
-	pair root;
+	explicit map(void);
+
+  private:
+	pair _root;
 };
+
+#include "../templates/Map.cpp"
 
 } /* namespace ft */

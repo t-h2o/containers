56edbdf579c0a679c9b18dd30b45df7ebc1174b5
|°_°| <grivel.theo@protonmail.com>
2023-01-05 01:40:39 +0100

    test: operator=

--- srcs/test_pair.cpp
+++ srcs/test_pair.cpp
@@ -20,6 +20,17 @@ pair_one(void)
 		ft::pair<int, int>	pair_ft_copy(pair_ft);
 		std::pair<int, int> pair_std_copy(pair_std);
 	}
+
+	{
+		ft::pair<int, int>	pair_ft(10, 42);
+		std::pair<int, int> pair_std(10, 42);
+
+		ft::pair<int, int>	pair_ft_copy = pair_ft;
+		std::pair<int, int> pair_std_copy = pair_std;
+
+		(void)pair_ft_copy;
+		(void)pair_std_copy;
+	}
 }
 
 void

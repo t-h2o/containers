09d502104f282f299361061648d35fa957aa88ab
To grivel <grivel.theo@protonmail.com>
2023-01-05 20:11:32 +0000

    test: map empty()

--- srcs/test_map.cpp
+++ srcs/test_map.cpp
@@ -4,18 +4,34 @@
 #include "color.hpp"
 #include <gtest/gtest.h>
 #include <iostream>
+#include <map>
+
+template <typename T1, typename T2>
+static void
+expected_equal(std::map<T1, T2> &map_std, ft::map<T1, T2> &map_ft)
+{
+	EXPECT_EQ(map_std.empty(), map_ft.empty());
+}
 
 static void
 map_one(void)
 {
 	section("test");
 
-	ft::map<int, int> tree;
+	std::map<int, int> map_std;
+	ft::map<int, int>  map_ft;
 
-	tree[42] = 100;
-	tree[12] = 200;
+	expected_equal(map_std, map_ft);
 
-	tree.print();
+	map_ft[42] = 100;
+	map_std[42] = 100;
+
+	expected_equal(map_std, map_ft);
+
+	map_ft[12] = 200;
+	map_std[12] = 200;
+
+	expected_equal(map_std, map_ft);
 }
 
 void

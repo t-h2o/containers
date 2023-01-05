8e19a4f2bce3c387fb8bb1b145b559a41d04f3cd
|°_°| <grivel.theo@protonmail.com>
2023-01-05 02:41:52 +0100

    Pair: swap()

--- headers/Pair.hpp
+++ headers/Pair.hpp
@@ -11,6 +11,8 @@ template <typename T1, typename T2> class pair
 
 	pair &operator=(const pair &);
 
+	void swap(pair &);
+
   private:
 	T1	  _first;
 	T2	  _second;
--- templates/Pair.cpp
+++ templates/Pair.cpp
@@ -24,3 +24,20 @@ pair<T1, T2>::operator=(pair const &other)
 
 	return *this;
 }
+
+template <typename T1, typename T2>
+void
+pair<T1, T2>::swap(pair &other)
+{
+	T1 tmp1;
+	T2 tmp2;
+
+	tmp1 = this->_first;
+	tmp2 = this->_second;
+
+	this->_first = other._first;
+	this->_second = other._second;
+
+	other._first = tmp1;
+	other._second = tmp2;
+}

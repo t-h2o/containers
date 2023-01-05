164669df0a15b6eb9668f31c04dca9c79141e446
|°_°| <grivel.theo@protonmail.com>
2023-01-05 01:40:26 +0100

    Pair: operator=

--- templates/Pair.cpp
+++ templates/Pair.cpp
@@ -11,3 +11,16 @@ pair<T1, T2>::pair(T1 const &first, T2 const &second)
 	: _first(first), _second(second)
 {
 }
+
+template <typename T1, typename T2>
+pair<T1, T2> &
+pair<T1, T2>::operator=(pair const &other)
+{
+	if (this == &other)
+		return *this;
+
+	this->_first = other._first;
+	this->_second = other._second;
+
+	return *this;
+}

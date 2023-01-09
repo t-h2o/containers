f8ff795a528963f162c07a8522def584dc696754
|°_°| <grivel.theo@protonmail.com>
2023-01-09 20:10:00 +0100

    map: refactor test

--- srcs/test_map.cpp
+++ srcs/test_map.cpp
@@ -14,7 +14,17 @@ expected_equal(std::map<T1, T2> &map_std, ft::map<T1, T2> &map_ft)
 	EXPECT_EQ(map_std.size(), map_ft.size());
 }
 
-static void
+template <typename T1, typename T2>
+void
+insert_map(int n, std::map<T1, T2> &map_std, ft::map<T1, T2> &map_ft)
+{
+	map_ft[n] = "";
+	map_std[n] = "";
+
+	expected_equal(map_std, map_ft);
+}
+
+void
 map_one(void)
 {
 	section("default constructor int, int");
@@ -22,160 +32,151 @@ map_one(void)
 	ft::map<int, std::string>  map_ft;
 
 	section("Init new pair 3 => ");
-
-	map_ft[3] = "";
-	map_std[3] = "";
-
-	expected_equal(map_std, map_ft);
+	insert_map(3, map_std, map_ft);
 
 	section("Init new pair 1 => ");
-
-	map_ft[1] = "";
-	map_std[1] = "";
-
-	expected_equal(map_std, map_ft);
+	insert_map(1, map_std, map_ft);
 
 	section("Init new pair 5 => ");
-
-	map_ft[5] = "";
-	map_std[5] = "";
-
-	expected_equal(map_std, map_ft);
+	insert_map(5, map_std, map_ft);
 
 	section("(Two red) Init new pair 7 => ");
-
-	map_ft[7] = "";
-	map_std[7] = "";
-
-	expected_equal(map_std, map_ft);
+	insert_map(7, map_std, map_ft);
 
 	section("(Two red) Init new pair 6 => ");
-
-	map_ft[6] = "";
-	map_std[6] = "";
-
-	expected_equal(map_std, map_ft);
+	insert_map(6, map_std, map_ft);
 
 	section("(Two red) Init new pair 8 => ");
-
-	map_ft[8] = "";
-	map_std[8] = "";
-
-	expected_equal(map_std, map_ft);
+	insert_map(8, map_std, map_ft);
 
 	section("(Two red) Init new pair 9 => ");
-
-	map_ft[9] = "";
-	map_std[9] = "";
-
-	expected_equal(map_std, map_ft);
+	insert_map(9, map_std, map_ft);
 
 	section("(Two red) Init new pair 10 => ");
-
-	map_ft[10] = "";
-	map_std[10] = "";
-
-	expected_equal(map_std, map_ft);
+	insert_map(10, map_std, map_ft);
 }
 
-static void
-map_two(void)
+void
+map_left(void)
 {
 	section("default constructor int, int");
 	std::map<int, std::string> map_std;
 	ft::map<int, std::string>  map_ft;
 
-	section("insert 9 ");
+	section("Init new pair 16 => ");
+	insert_map(16, map_std, map_ft);
 
-	map_ft[9] = "";
-	map_std[9] = "";
+	section("Init new pair 15 => ");
+	insert_map(15, map_std, map_ft);
 
-	expected_equal(map_std, map_ft);
+	section("Init new pair 14 => ");
+	insert_map(14, map_std, map_ft);
 
-	section("insert 8 ");
+	section("Init new pair 13 => ");
+	insert_map(13, map_std, map_ft);
 
-	map_ft[8] = "";
-	map_std[8] = "";
+	section("Init new pair 12 => ");
+	insert_map(12, map_std, map_ft);
 
-	expected_equal(map_std, map_ft);
+	section("Init new pair 11 => ");
+	insert_map(11, map_std, map_ft);
 
-	section("insert 7 ");
+	section("Init new pair 10 => ");
+	insert_map(10, map_std, map_ft);
 
-	map_ft[7] = "";
-	map_std[7] = "";
+	section("Init new pair 9 => ");
+	insert_map(9, map_std, map_ft);
 
-	expected_equal(map_std, map_ft);
+	section("Init new pair 8 => ");
+	insert_map(8, map_std, map_ft);
 
-	section("insert 6 ");
+	section("Init new pair 7 => ");
+	insert_map(7, map_std, map_ft);
 
-	map_ft[6] = "";
-	map_std[6] = "";
+	section("Init new pair 6 => ");
+	insert_map(6, map_std, map_ft);
 
-	expected_equal(map_std, map_ft);
+	section("Init new pair 5 => ");
+	insert_map(5, map_std, map_ft);
 
-	section("insert 5 ");
+	section("Init new pair 4 => ");
+	insert_map(4, map_std, map_ft);
 
-	map_ft[5] = "";
-	map_std[5] = "";
+	section("Init new pair 3 => ");
+	insert_map(3, map_std, map_ft);
 
-	expected_equal(map_std, map_ft);
+	section("Init new pair 2 => ");
+	insert_map(2, map_std, map_ft);
 
-	section("insert 4 ");
+	section("Init new pair 1 => ");
+	insert_map(1, map_std, map_ft);
+}
 
-	map_ft[4] = "";
-	map_std[4] = "";
+void
+map_right(void)
+{
+	section("default constructor int, int");
+	std::map<int, std::string> map_std;
+	ft::map<int, std::string>  map_ft;
 
-	expected_equal(map_std, map_ft);
+	section("Init new pair 1 => ");
+	insert_map(1, map_std, map_ft);
 
-	section("insert 3 ");
+	section("Init new pair 2 => ");
+	insert_map(2, map_std, map_ft);
 
-	map_ft[3] = "";
-	map_std[3] = "";
+	section("Init new pair 3 => ");
+	insert_map(3, map_std, map_ft);
 
-	expected_equal(map_std, map_ft);
+	section("Init new pair 4 => ");
+	insert_map(4, map_std, map_ft);
 
-	section("insert 2 ");
+	section("Init new pair 5 => ");
+	insert_map(5, map_std, map_ft);
 
-	map_ft[2] = "";
-	map_std[2] = "";
+	section("Init new pair 6 => ");
+	insert_map(6, map_std, map_ft);
 
-	expected_equal(map_std, map_ft);
+	section("Init new pair 7 => ");
+	insert_map(7, map_std, map_ft);
 
-	section("insert 1 ");
+	section("Init new pair 8 => ");
+	insert_map(8, map_std, map_ft);
 
-	map_ft[1] = "";
-	map_std[1] = "";
+	section("Init new pair 9 => ");
+	insert_map(9, map_std, map_ft);
 
-	expected_equal(map_std, map_ft);
+	section("Init new pair 10 => ");
+	insert_map(10, map_std, map_ft);
 
-	section("insert 0 ");
+	section("Init new pair 11 => ");
+	insert_map(11, map_std, map_ft);
 
-	map_ft[0] = "";
-	map_std[0] = "";
+	section("Init new pair 12 => ");
+	insert_map(12, map_std, map_ft);
 
-	expected_equal(map_std, map_ft);
+	section("Init new pair 13 => ");
+	insert_map(13, map_std, map_ft);
 
-	section("insert -1 ");
+	section("Init new pair 14 => ");
+	insert_map(14, map_std, map_ft);
 
-	map_ft[-1] = "";
-	map_std[-1] = "";
+	section("Init new pair 15 => ");
+	insert_map(15, map_std, map_ft);
 
-	expected_equal(map_std, map_ft);
-
-	section("insert -2 ");
-
-	map_ft[-2] = "";
-	map_std[-2] = "";
-
-	expected_equal(map_std, map_ft);
+	section("Init new pair 16 => ");
+	insert_map(16, map_std, map_ft);
 }
 
 void
 test_map(void)
 {
-	title("Map 1");
+	title("Map one");
 	map_one();
 
-	title("Map 2");
-	map_two();
+	title("Map left");
+	map_left();
+
+	title("Map right");
+	map_right();
 }
